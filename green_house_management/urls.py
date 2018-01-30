f29922fe588ac89ad28b81bc480bd00ef7009a43
Sateesh Kadiyala <sk@Monicas-MacBook-Pro.local>
2018-01-29 23:41:06 -0600

    adding a bootstrap template and adding pages to list plots, devices and media

--- green_house_management/urls.py
+++ green_house_management/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('plots', views.plots_list, name='plots'),
+    path('devices', views.devices_list, name='devices'),
+    path('media', views.media_list, name='media'),
 ]
--- green_house_management/views.py
+++ green_house_management/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import *
 
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("Green House Management..! Meter Exercise")
+def plots_list(request):
+
+    plots = Plot.objects.all()
+    t = loader.get_template('green_house_management/plots.html')
+    c = {'plots' : plots}
+    return HttpResponse(t.render(c, request))
+
+
+def media_list(request):
+
+    media = Media.objects.all()
+    t = loader.get_template('green_house_management/media.html')
+    c = {'media' : media}
+    return HttpResponse(t.render(c, request))
+
+
+def devices_list(request):
+
+    devices = Device.objects.all()
+    t = loader.get_template('green_house_management/devices.html')
+    c = {'devices' : devices}
+    return HttpResponse(t.render(c, request))
